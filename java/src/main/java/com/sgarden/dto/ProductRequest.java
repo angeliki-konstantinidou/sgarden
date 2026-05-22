16c309e915171c9a055c8f8152c9852b65cbf462
Angeliki Konstantinidou <angeliki@net2grid.com>
2026-05-22 13:16:51 +0300

    test
    test

--- java/src/main/java/com/sgarden/dto/ProductRequest.java
+++ java/src/main/java/com/sgarden/dto/ProductRequest.java
@@ -1,17 +1,32 @@
 package com.sgarden.dto;
 
+import com.sgarden.validation.NullOrNotBlank;
+import com.sgarden.validation.OnCreate;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class ProductRequest {
 
+    @NotBlank(groups = OnCreate.class, message = "name is required")
+    @NullOrNotBlank(message = "name must not be blank")
+    @Size(max = 100, message = "name must not exceed 100 characters")
     private String name;
 
+    @Size(max = 500, message = "description must not exceed 500 characters")
     private String description;
 
+    @Size(max = 50, message = "category must not exceed 50 characters")
     private String category;
 
+    @NotNull(groups = OnCreate.class, message = "price is required")
+    @Positive(message = "price must be greater than 0")
     private Double price;
 
+    @PositiveOrZero(message = "stock must be 0 or greater")
     private Integer stock;
 }
---
+++ java/src/main/java/com/sgarden/validation/OnCreate.java
@@ -0,0 +1,5 @@
+package com.sgarden.validation;
+
+import jakarta.validation.groups.Default;
+
+public interface OnCreate extends Default {}
